edf4b7dc972b7e567a58fa97a11175dbb4cef6ee
avolfer <jakubstuba2004@gmail.com>
2023-01-11 14:58:53 +0100

    change code

--- 1app.js
+++ 1app.js
@@ -1,5 +1,27 @@
-function multiply(a, b) {
-    return a * b;
+function checkDriverAge(age) {
+    if (Number(age) < 18) {
+        return "Sorry, you are too young";
+    } else if (Number(age) > 18) {
+        return "Powering On. Enjoy the ride!";
+    } else if (Number(age) === 18) {
+        return "Congratulations on your first year of driving. Enjoy the ride!";
+    }
 }
 
-multiply(10, 10)
+console.log(checkDriverAge(92));
+
+
+let checkDriverAge2 = function() {
+    let age = prompt("What is your age?");
+    if (Number(age) < 18) {
+        alert("Sorry, you are too yound to drive this car. Powering off");
+    } else if (Number(age) > 18) {
+        alert("Powering On. Enjoy the ride!");
+    } else if (Number(age) === 18) {
+        alert("Congratulations on your first year of driving. Enjoy the ride!");
+    }
+}
+
+checkDriverAge2();
+
+
